fcdd2504f88a788b07f20abc9dd02eb1d3bfdd3d
GrigoriyKruchinin <GrigoriyKruchinin>
2023-10-24 11:02:32 +0200

    .

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    articles = ['Буратино утонул', 'Колобок повесился', 'Русалка села на шпагат']
-    return render(request, 'articles/index.html', context={'articles': articles})
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        articles = ['Буратино утонул', 'Колобок повесился', 'Русалка села на шпагат']
+        return render(request, 'articles/index.html', context={'articles': articles})
